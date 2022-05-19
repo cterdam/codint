3b857eaf5dd87e75fee44c682ad623a504dc392f
cterdam <llz.joshlee@gmail.com>
2022-05-19 23:52:56 +0800

    finish packaging existing problems

--- vowel.py
+++
@@ -1,6 +0,0 @@
-# Input: a string with only English letters and whitespace
-# Task: return the string with vowels capitalized and consonants decapitalized
-
-def vowel(line):
-    vowels = ('a', 'e', 'i', 'o', 'u')
-    return "".join([x.upper() if x in vowels else x.lower() for x in line])
